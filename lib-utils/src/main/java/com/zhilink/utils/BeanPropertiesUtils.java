5ec8b254ee4707405940ac2cb53f3515ea2bfad3
谢孟 <xm@123.com>
2018-10-16 19:17:57 +0800

    1.0.3bean复制支持父类复制更新

--- lib-utils/src/main/java/com/zhilink/utils/BeanPropertiesUtils.java
+++ lib-utils/src/main/java/com/zhilink/utils/BeanPropertiesUtils.java
@@ -1,11 +1,18 @@
 package com.zhilink.utils;
 
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 对象copy
@@ -16,14 +23,14 @@ import java.util.List;
  */
 public class BeanPropertiesUtils {
     /**
-     * 利用反射实现对象之间属性复制
+     * 利用反射实现对象之间属性复制--父类属性无法copy
      */
     public static void copyProperties(Object from, Object to) throws Exception {
         copyPropertiesExclude(from, to, null);
     }
 
     /**
-     * 复制对象属性
+     * 复制对象属性--父类属性无法copy
      */
     @SuppressWarnings("unchecked")
     public static void copyPropertiesExclude(Object from, Object to, String[] excludesArray) {
@@ -73,11 +80,12 @@ public class BeanPropertiesUtils {
         }
     }
 
+
     /**
-     * 对象属性值复制，仅复制指定名称的属性值
+     * 对象属性值复制，仅复制指定名称的属性值 --父类属性无法copy
      */
     @SuppressWarnings("unchecked")
-    public static void copyPropertiesInclude(Object from, Object to, String[] includesArray) {
+    public static void copyPropertiesInclude(Class from, Class to, String[] includesArray) {
         List<String> includesList = null;
         if (includesArray != null && includesArray.length > 0) {
             includesList = Arrays.asList(includesArray);    //构造列表对象
@@ -138,4 +146,87 @@ public class BeanPropertiesUtils {
         }
         return null;
     }
+
+
+    /**
+     * 复制字段的值
+     *
+     * @param source 源对象
+     * @param target 目标对象
+     */
+    public static void copyFields(Object source, Object target) {
+        HashMap<String, Field> sourceFieldMap = getAllFields(source);
+        HashMap<String, Field> targetFieldMap = getAllFields(target);
+        Iterator entries = sourceFieldMap.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry entry = (Map.Entry) entries.next();
+            String key = (String) entry.getKey();
+            Field value = (Field) entry.getValue();
+            value.setAccessible(true);
+            try {
+                if (value.get(source) != null && targetFieldMap.containsKey(key)) {
+                    targetFieldMap.get(key).setAccessible(true);
+                    targetFieldMap.get(key).set(target, value.get(source));
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * 复制字段的值
+     *
+     * @param source         源对象
+     * @param target         目标对象
+     * @param skipFieldNames 过滤的字段
+     */
+    public static void copyFields(Object source, Object target, String[] skipFieldNames) {
+        Set<String> skips = new HashSet<>();
+        if (skipFieldNames != null) {
+            skips.addAll(Arrays.asList(skipFieldNames));
+        }
+        HashMap<String, Field> sourceFieldMap = getAllFields(source);
+        HashMap<String, Field> targetFieldMap = getAllFields(target);
+        Iterator entries = sourceFieldMap.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry entry = (Map.Entry) entries.next();
+            String key = (String) entry.getKey();
+            Field value = (Field) entry.getValue();
+            value.setAccessible(true);
+            try {
+                if (value.get(source) != null && !skips.contains(key) && targetFieldMap.containsKey(key)) {
+                    targetFieldMap.get(key).setAccessible(true);
+                    targetFieldMap.get(key).set(target, value.get(source));
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * 获取对象所有的字段 包括父类
+     *
+     * @param obj 对象
+     * @return 字段名与Field对象的映射map
+     */
+    private static HashMap<String, Field> getAllFields(Object obj) {
+        Class sourceClass = obj.getClass();
+        //获取对象所有字段 包括父类
+        ArrayList<Field> sourceFields = new ArrayList<>();
+        while (sourceClass != null) {
+            sourceFields.addAll(Arrays.asList(sourceClass.getDeclaredFields()));
+            sourceClass = sourceClass.getSuperclass();
+        }
+        //字段名去重
+        HashMap<String, Field> sourceFieldMap = new HashMap<>(16);
+        for (Field field : sourceFields) {
+            sourceFieldMap.put(field.getName(), field);
+        }
+        return sourceFieldMap;
+    }
+
 }
