6cdf5551e833bfefa2c96f236e56490882750b3a
谢孟 <xm@123.com>
2019-04-04 15:27:32 +0800

    注释

--- lib-utils/src/main/java/com/zhilink/utils/BeanPropertiesUtils.java
+++ lib-utils/src/main/java/com/zhilink/utils/BeanPropertiesUtils.java
@@ -40,9 +40,12 @@ public class BeanPropertiesUtils {
     }
 
     /**
-     * 复制对象属性
+     * @param from          from
+     * @param to            to
+     * @param excludesArray 排出的字段
+     * @param isCover       为false时，且字段不为空时，不覆盖该字段
+     *                      为true时，直接覆盖所有字段
      */
-    @SuppressWarnings("unchecked")
     public static void copyPropertiesExcludeCover(Object from, Object to, String[] excludesArray, boolean isCover) {
         if (null == from || to == null) {
             return;
@@ -105,7 +108,6 @@ public class BeanPropertiesUtils {
                     }
                 }
                 Object oldValue = toGetMethod.invoke(to, new Object[0]);
-                //如果
                 if (!isCover && null != oldValue && !"0".equals(oldValue.toString())
                         && !StringUtils.isBlank(oldValue.toString())
                         && !"false".equals(oldValue.toString())) {
@@ -121,73 +123,6 @@ public class BeanPropertiesUtils {
         }
     }
 
-    /**
-     * 对象属性值复制，仅复制指定名称的属性值 --父类属性无法copy
-     */
-    @SuppressWarnings("unchecked")
-    public static void copyPropertiesInclude(Class from, Class to, String[] includesArray) {
-        List<String> includesList = null;
-        if (includesArray != null && includesArray.length > 0) {
-            includesList = Arrays.asList(includesArray);
-        } else {
-            return;
-        }
-
-        Class sourceClass = from.getClass();
-        ArrayList fromMethodList;
-        for (fromMethodList = new ArrayList(); sourceClass != null; sourceClass = sourceClass.getSuperclass()) {
-            fromMethodList.addAll(Arrays.asList(sourceClass.getDeclaredMethods()));
-        }
-
-        Class toClass = to.getClass();
-        ArrayList toMethodList;
-        for (toMethodList = new ArrayList(); toClass != null; toClass = toClass.getSuperclass()) {
-            toMethodList.addAll(Arrays.asList(toClass.getDeclaredMethods()));
-        }
-        Method[] fromMethods = new Method[fromMethodList.size()];
-        fromMethodList.toArray(fromMethods);
-        Method[] toMethods = new Method[toMethodList.size()];
-        toMethodList.toArray(toMethods);
-
-        Method fromMethod = null, toMethod = null;
-        String fromMethodName = null, toMethodName = null;
-        for (int i = 0; i < fromMethods.length; i++) {
-            fromMethod = fromMethods[i];
-            fromMethodName = fromMethod.getName();
-            if (!fromMethodName.contains("get")) {
-                continue;
-            }
-            //排除列表检测
-            String str = fromMethodName.substring(3);
-            if (!includesList.contains(str.substring(0, 1).toLowerCase() + str.substring(1))) {
-                continue;
-            }
-            toMethodName = "set" + fromMethodName.substring(3);
-            toMethod = findMethodByName(toMethods, toMethodName);
-            if (toMethod == null) {
-                continue;
-            }
-            Object value = null;
-            try {
-                value = fromMethod.invoke(from, new Object[0]);
-                if (value == null) {
-                    continue;
-                }
-                //集合类判空处理
-                if (value instanceof Collection) {
-                    Collection newValue = (Collection) value;
-                    if (newValue.size() <= 0) {
-                        continue;
-                    }
-                }
-                toMethod.invoke(to, new Object[]{value});
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
 
     /**
